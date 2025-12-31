2a0c617e1f1c6f778c71f148cd4fb9aa5dcbea81
Simon Racko <rackosimon@proton.me>
2025-12-31 20:22:50 +0100

    Update metadata title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Autoškola Martinec - Cesta začíná zde',
-  description: 'Prémiová autoškola s pobočkami v Bystřici pod Hostýnem a Přerově',
+  title: 'Autoškola Martinec',
+  description: 'Moderní autoškola s pobočkami v Bystřici pod Hostýnem a Přerově. Kvalitní výuka, individuální přístup a vysoká úspěšnost u zkoušek.',
   icons: {
     icon: [
       { url: '/images/loga/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
