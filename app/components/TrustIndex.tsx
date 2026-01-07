90863b2cc43c3177b0baa8a93be63692fd0a37e4
Simon Racko <rackosimon@proton.me>
2026-01-07 12:56:05 +0100

    Update Trustindex widget script ID to 95f82ff61b54749411564affa0e

--- app/components/TrustIndex.tsx
+++ app/components/TrustIndex.tsx
@@ -12,7 +12,7 @@ export default function TrustIndex() {
     ref.current.innerHTML = '';
 
     const script = document.createElement('script');
-    script.src = 'https://cdn.trustindex.io/loader.js?dc3adaf611e974869d96a18e73b';
+    script.src = 'https://cdn.trustindex.io/loader.js?95f82ff61b54749411564affa0e';
     script.async = true;
     script.defer = true;
 
